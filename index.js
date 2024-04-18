55b24ed78992f943765f04e7036311b721c72d02
Kaleb A Butler <kaleb.butler2010@gmail.com>
2024-04-18 18:52:46 -0500

    lab work

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 
-var firstName = 'Kaleb'
-var  lastName = 'Butler'
-var fullName = firstName + " "+ lastName
- console.log(fullName)
+/-console.log("Step 1:  Number of Seats on a Plane");-/
+var numberofSeats = 200;
+console.log(numberofSeats);
+
+/-console.log("Step 2:  Cost of Groceries");-/
+var costofGroceries = 0.00;
+console.log(costofGroceries);
+
+console.log("Step 3:  Middle Initial");
+let middleInitial = 'a';
+console.log(middleInitial);
+
+console.log("Step 4:  Is it Hot Outside?");
+var temperature = 75;
+if (temperature > 75) {
+    console.log("It is Hot Outside!");
+}else{
+    console.log("It is not Hot Outside");
+};
+
+
+console.log("Step 5:  First Name");
+let firstName ='john';
+console.log(firstName);
+
+console.log("Step 6:  Street Address");
+let streetAddress = "123 Main St";
+console.log(streetAddress);
