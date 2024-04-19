47175d6475c3dcc109d09a6dbd0693fc4e864d6c
Kaleb A Butler <kaleb.butler2010@gmail.com>
2024-04-19 10:07:09 -0500

    Finish editing my lab

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var costofGroceries = 0.00;
 console.log(costofGroceries);
 
 console.log("Step 3:  Middle Initial");
-let middleInitial = 'a';
+let middleInitial = "F";
 console.log(middleInitial);
 
 console.log("Step 4:  Is it Hot Outside?");
@@ -27,3 +27,47 @@ console.log(firstName);
 console.log("Step 6:  Street Address");
 let streetAddress = "123 Main St";
 console.log(streetAddress);
+
+
+console.log('Remove Plane Seats:');
+//	A customer booked 2 plane seats. Remove 2 seats from the available seats variable
+//  ↓ YOUR CODE HERE ↓
+var numberofSeats = 200;
+console.log(numberofSeats - 2);
+
+console.log('Buy a Candy Bar:');
+//	Impulse candy bar purchase! Add 2.15 to the grocery total
+//  ↓ YOUR CODE HERE ↓
+var costofGroceries = 0.00;
+console.log(costofGroceries + 2.15);
+
+    console.log('Fix a Birth Certificate:');
+//	A birth certificate was printed incorrectly, change the middle initial to something else
+//  ↓ YOUR CODE HERE ↓
+const originalString ="F"
+const newString = originalString.replace(/F/i, "A");
+console.log(newString);
+    console.log('Changing Seasons:');
+//	The season has changed, update the hot outside variable	to be opposite of what it was
+//  ↓ YOUR CODE HERE ↓
+var temperature = 90;
+if (temperature > 75) {
+    console.log("It is Hot Outside!");
+}else{
+    console.log("It is not Hot Outside");
+};
+
+    console.log('Full Name:');
+//	Create a new variable called full name using the customer's
+//  first name, the middle initial, and a last name of your choice
+//  ↓ YOUR CODE HERE ↓
+var lastName = "smith";
+var fullName = firstName + " " + newString + " " + lastName;
+console.log(fullName);
+
+
+    console.log('Introduce a Customer:');
+//	Print a line to the console that introduces the customer 
+//	including the name, and saying that they live at the address variable
+//  ↓ YOUR CODE HERE ↓
+console.log("This is " + fullName + " who lives at " + streetAddress);
